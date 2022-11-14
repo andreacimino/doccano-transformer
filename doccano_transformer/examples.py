8e6411b13855eee2b8fcc44c8c711a2d18669a87
Andrea Cimino <andrea.cimino@errequadrosrl.com>
2022-11-14 11:06:29 +0100

    Improvements

--- doccano_transformer/examples.py
+++ doccano_transformer/examples.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
 from typing import Callable, Iterator, List, Optional
-
-from spacy.gold import biluo_tags_from_offsets
+from cleantext import clean
 
 from doccano_transformer import utils
 
@@ -12,37 +11,37 @@ class Example:
 
 
 class NERExample:
-
     def __init__(self, raw: dict) -> None:
         self.raw = raw
-        self.id = raw['id']
-        self.text = raw['text']
-        self.sentences = utils.split_sentences(raw['text'])
-        self.sentence_offsets = utils.get_offsets(raw['text'], self.sentences)
-        self.sentence_offsets.append(len(raw['text']))
+        self.id = raw["id"]
+        self.text = raw["text"]
+        self.sentences = utils.split_sentences(raw["text"])
+        self.sentences = [x for x in self.sentences if len(x.strip()) > 0]
+        self.sentence_offsets = utils.get_offsets(raw["text"], self.sentences)
+        self.sentence_offsets.append(len(raw["text"]))
 
     @property
     def labels(self):
-        if 'annotations' in self.raw:
+        if "annotations" in self.raw:
             labels = defaultdict(list)
-            for annotation in self.raw['annotations']:
-                labels[annotation['user']].append([
-                    annotation['start_offset'],
-                    annotation['end_offset'],
-                    annotation['label']
-                ])
+            for annotation in self.raw["annotations"]:
+                labels["DEFAULT"].append(
+                    [
+                        annotation["start_offset"],
+                        annotation["end_offset"],
+                        annotation["label"],
+                    ]
+                )
             return labels
-        elif 'labels' in self.raw:
+        elif "label" in self.raw:
             labels = defaultdict(list)
-            for label in self.raw['labels']:
+            for label in self.raw["label"]:
                 # TODO: This format doesn't have a user field currently.
                 # So this method uses the user 0 for all label.
                 labels[0].append(label)
             return labels
         else:
-            raise KeyError(
-                'The file should includes either "labels" or "annotations".'
-            )
+            raise KeyError('The file should includes either "labels" or "annotations".')
 
     def get_tokens_and_token_offsets(self, tokenizer):
         tokens = [tokenizer(sentence) for sentence in self.sentences]
@@ -57,55 +56,57 @@ class NERExample:
     def is_valid(self, raise_exception: Optional[bool] = True) -> bool:
         return True
 
-    def to_conll2003(
-        self, tokenizer: Callable[[str], List[str]]
-    ) -> Iterator[dict]:
-        all_tokens, all_token_offsets = self.get_tokens_and_token_offsets(
-            tokenizer)
+    def to_conll2003(self, tokenizer: Callable[[str], List[str]]) -> Iterator[dict]:
+        all_tokens, all_token_offsets = self.get_tokens_and_token_offsets(tokenizer)
         for user, labels in self.labels.items():
             label_split = [[] for _ in range(len(self.sentences))]
             for label in labels:
                 for i, (start, end) in enumerate(
-                        zip(self.sentence_offsets, self.sentence_offsets[1:])):
+                    zip(self.sentence_offsets, self.sentence_offsets[1:])
+                ):
                     if start <= label[0] <= label[1] <= end:
                         label_split[i].append(label)
-            lines = ['-DOCSTART- -X- -X- O\n\n']
+            # lines = ["-DOCSTART- -X- -X- O\n\n"]
+            lines = []
+
             for tokens, offsets, label in zip(
-                    all_tokens, all_token_offsets, label_split):
+                all_tokens, all_token_offsets, label_split
+            ):
                 tags = utils.create_bio_tags(tokens, offsets, label)
                 for token, tag in zip(tokens, tags):
-                    lines.append(f'{token} _ _ {tag}\n')
-                lines.append('\n')
-            yield {'user': user, 'data': ''.join(lines)}
+                    token = clean(
+                        token,
+                        no_emoji=True,
+                        fix_unicode=True,
+                        lower=False,  # Convert to lowercase
+                    )  # Remove all punctuations)
+                    lines.append(f"{token} _ _ {tag}\n")
+                lines.append("\n")
+            yield {"user": user, "data": "".join(lines)}
 
-    def to_spacy(
-        self, tokenizer: Callable[[str], List[str]]
-    ) -> Iterator[dict]:
-        all_tokens, all_token_offsets = self.get_tokens_and_token_offsets(
-            tokenizer)
+    def to_spacy(self, tokenizer: Callable[[str], List[str]]) -> Iterator[dict]:
+        all_tokens, all_token_offsets = self.get_tokens_and_token_offsets(tokenizer)
         for user, labels in self.labels.items():
             label_split = [[] for _ in range(len(self.sentences))]
             for label in labels:
                 for i, (start, end) in enumerate(
-                        zip(self.sentence_offsets, self.sentence_offsets[1:])):
+                    zip(self.sentence_offsets, self.sentence_offsets[1:])
+                ):
                     if start <= label[0] <= label[1] <= end:
                         label_split[i].append(label)
 
-            data = {'raw': self.text}
+            data = {"raw": self.text}
             sentences = []
             for tokens, offsets, label in zip(
-                    all_tokens, all_token_offsets, label_split):
+                all_tokens, all_token_offsets, label_split
+            ):
                 tokens = utils.convert_tokens_and_offsets_to_spacy_tokens(
                     tokens, offsets
                 )
                 tags = biluo_tags_from_offsets(tokens, label)
                 tokens_for_spacy = []
-                for i, (token, tag, offset) in enumerate(
-                    zip(tokens, tags, offsets)
-                ):
-                    tokens_for_spacy.append(
-                        {'id': i, 'orth': str(token), 'ner': tag}
-                    )
-                sentences.append({'tokens': tokens_for_spacy})
-            data['sentences'] = sentences
-            yield {'user': user, 'data': {'id': self.id, 'paragraphs': [data]}}
+                for i, (token, tag, offset) in enumerate(zip(tokens, tags, offsets)):
+                    tokens_for_spacy.append({"id": i, "orth": str(token), "ner": tag})
+                sentences.append({"tokens": tokens_for_spacy})
+            data["sentences"] = sentences
+            yield {"user": user, "data": {"id": self.id, "paragraphs": [data]}}
